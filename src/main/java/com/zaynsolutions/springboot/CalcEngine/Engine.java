1114576fea03628806eacbc3a24e390eaf288fd8
Root User <root@localhost>
2017-12-23 01:54:54 +0500

    =first commit

---
+++ src/main/java/com/zaynsolutions/springboot/CalcEngine/Engine.java
@@ -0,0 +1,23 @@
+package com.zaynsolutions.springboot.CalcEngine;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class Engine 
+{
+	
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "This is a Calc Engine and its used to perform complex calculations!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Engine.class, args);
+    }
+
+}
