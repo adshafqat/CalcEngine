d3534e55468e05123eddc9c21d45f4033f64e58c
adshafqat <adshafqat@yahoo.com>
2019-06-29 22:51:22 +0100

    Modified Calc Engine Contoller

--- src/main/java/com/zaynsolutions/springboot/CalcEngine/Engine.java
+++ src/main/java/com/zaynsolutions/springboot/CalcEngine/Engine.java
@@ -1,17 +1,14 @@
 package com.zaynsolutions.springboot.CalcEngine;
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableAutoConfiguration
+@RestController
+@RequestMapping("/calcengine")
 public class Engine 
 {
 	
-
-    @RequestMapping("/engine")
-    @ResponseBody
+    @GetMapping
     String home() {
         return "This is a Calc Engine and its used to perform complex calculations!";
     }
